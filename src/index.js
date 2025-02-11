50bb9c8d9e0b119d56535ed1026e00ff472874d0
sofia11244 <denizsofia03@gmail.com>
2025-02-11 15:26:25 +0300

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // src/index.js
 
+import { initMongoDB } from './db/initMongoDB.js';
 import { SetupServer } from './server.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const bootstrap = async () => {
+  await initMongoDB();
   SetupServer();
 };
 
