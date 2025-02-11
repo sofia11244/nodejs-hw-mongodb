220a32427fa2b238bfc80977cba71acc22f57761
sofia11244 <denizsofia03@gmail.com>
2025-02-11 14:40:54 +0300

    .

--- src/server.js
+++ src/server.js
@@ -59,6 +59,6 @@ export const SetupServer = () => {
   });
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port "${PORT}"`);
   });
 };
