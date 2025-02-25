31c3c4e69643131046257184b2f8ae66f818caef
sofia11244 <denizsofia03@gmail.com>
2025-02-25 15:43:54 +0300

    .

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,20 +1,18 @@
+// src/routers/auth.js
+
 import { Router } from 'express';
-import { validateBody } from '../middlewares/validateBody.js';
-import {
-  loginUserSchema,
-  registerUserSchema,
-  requestResetEmailSchema,
-  resetPasswordSchema,
-} from '../validation/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import {
-  loginUserController,
-  logoutUserController,
-  refreshUserSessionController,
-  registerUserController,
-  requestResetEmailController,
-  resetPasswordController,
-} from '../controllers/auth.js';
+import { registerUserSchema } from '../validation/auth.js';
+import { registerUserController } from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { loginUserSchema} from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
+import { logoutUserController } from '../controllers/auth.js';
+import { refreshUserSessionController } from '../controllers/auth.js';
+import { requestResetEmailSchema } from '../validation/auth.js';
+import { requestResetEmailController } from '../controllers/auth.js';
+import { resetPasswordSchema } from '../validation/auth.js';
+import { resetPasswordController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -24,25 +22,26 @@ router.post(
   ctrlWrapper(registerUserController),
 );
 
-router.post(
-  '/login',
-  validateBody(loginUserSchema),
-  ctrlWrapper(loginUserController),
-);
-
-router.post('/logout', ctrlWrapper(logoutUserController));
-router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 router.post(
-  '/send-reset-email',
-  validateBody(requestResetEmailSchema),
-  ctrlWrapper(requestResetEmailController),
-);
+    '/login',
+    validateBody(loginUserSchema),
+    ctrlWrapper(loginUserController),
+  );
 
-router.post(
-  '/reset-pwd',
-  validateBody(resetPasswordSchema),
-  ctrlWrapper(resetPasswordController),
-);
+  router.post('/logout', ctrlWrapper(logoutUserController));
 
+  router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+  router.post(
+    '/request-reset-email',
+    validateBody(requestResetEmailSchema),
+    ctrlWrapper(requestResetEmailController),
+  );
+
+  router.post(
+    '/reset-password',
+    validateBody(resetPasswordSchema),
+    ctrlWrapper(resetPasswordController),
+  );
 export default router;
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,38 +1,83 @@
+
 import { Router } from 'express';
-import {
-  createSendContact,
-  deleteSendContact,
-  getSendAllContacts,
-  getSendContactById,
-  updateSendContact,
-} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import {
-  createContactSchema,
-  updateContactSchema,
-} from '../validation/contacts.js';
-import { isValidId } from '../middlewares/isValidID.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { updateContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { upload } from '../middlewares/multer.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
+
+
+import {
+    getAllContactsController,
+getContactsByIdController,
+createContactController,
+deleteContactController,
+upsertContactController,
+patchContactController
+} from '../controllers/contacts.js';
+
 
 const router = Router();
-router.use(authenticate);
-router.get('/', ctrlWrapper(getSendAllContacts));
-router.get('/:contactId', isValidId, ctrlWrapper(getSendContactById));
+
+// Çünkü server.js içinde zaten /contacts yolunu verdin bu yüzden conatcs eklemene gerek yok.
+
+router.get('/', ctrlWrapper(getAllContactsController));
+
+router.get('/:contactId', isValidId, ctrlWrapper(getContactsByIdController));
+
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+router.put('/:contactId', ctrlWrapper(upsertContactController));
+
+router.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+
 router.post(
-  '/',
-  upload.single('photo'),
-  validateBody(createContactSchema),
-  ctrlWrapper(createSendContact),
-);
-router.patch(
-  '/:contactId',
-  isValidId,
-  upload.single('photo'),
-  validateBody(updateContactSchema),
-  ctrlWrapper(updateSendContact),
-);
-router.delete('/:contactId', isValidId, ctrlWrapper(deleteSendContact));
+    '/register',
+    validateBody(createContactSchema),
+    ctrlWrapper(createContactController),
+  );
+
+  router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsByIdController));
+
+
 
 export default router;
+
+
+import { upload } from '../middlewares/multer.js';
+
+/* Dosyanın diğer kodları */
+
+router.post(
+  '/',
+  checkRoles(ROLES.USER), 
+  isValidId,
+  upload.single('photo'), // bu middleware'i ekliyoruz
+  validateBody(createContactSchema), 
+  ctrlWrapper(createContactController), 
+);
+
+router.put(
+  '/:contactId',
+  checkRoles(ROLES.USER),
+  isValidId,
+  upload.single('photo'), // bu middleware'i ekliyoruz
+  validateBody(createContactSchema), 
+  ctrlWrapper(upsertContactController),
+);
+
+router.patch(
+  '/:contactId',
+  checkRoles(ROLES.USER),
+  isValidId,
+  upload.single('photo'), // bu middleware'i ekliyoruz
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController), 
+);
